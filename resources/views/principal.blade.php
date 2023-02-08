8b028d524f3037be59fbe43e39e93aa6c60183d5
Carlos Souza <carlos.souza@unigranrio.br>
2023-02-08 07:04:38 -0300

    Atualização dos cards

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.main')
 @section('title', 'Principal')
 @section('content')
-    <div id="sub-menu" class="col-md-6">
+    <div id="sub-menu" class="col-md-12">
         <a href="#">Em aberto</a>
         <a href="#">Respondidos</a>
         <a href="#">Todos</a>
     </div>
-    <div id="questionario" class="col-md-6 offset-md-1">
-        <th>ID</th>
-            <th>Pergunta</th>
-            <th>Resposta</th>
-            <th>Complemento</th>
-            <th>Data da resposta</th>
-            <th>Quem respondeu</th>
-        <ul>
+    <div id="painel" class="col-md-12 offset-md-1">
+        <div id="card-conteiner" class="row">
             @foreach ($entradas as $entrada)
-                <li>{{ $entrada['componente']}}</li>
+                <p>{{ $entrada['componente'] }}</p>
             @endforeach
             @foreach ($questions as $question)
-                <li>{{ $question['id']}} {{$question['pergunta']}}</li>
+                <div id="card" class="card col-md-2">
+                    <p>Grau de risco:</p>
+                    <div class="grisco"></div>
+                    <div id="card-content">
+                        <p>{{ $question['id'] }} {{ $question['pergunta'] }}</p>
+                        <p>Cliente: Termaco</p>
+                        <p>Ultimas Atividades: 08/02/2023</p>
+                        <p>Colaborador: Maria Luciana</p>
+                        <p>Evolução: 10%</p>
+                    </div>
+                </div>
             @endforeach
-        </ul>
+        </div>
     </div>
 
 @endsection
